aea4e18730836aa1a9a5cbd4ae1c24d17630f3e2
iamkentleom <leomakibulan@gmail.com>
2021-05-20 10:42:41 +0800

    fix(QuizScreen): remove duplicate declaration

--- lib/Screens/QuizScreen.dart
+++ lib/Screens/QuizScreen.dart
@@ -20,9 +20,8 @@ class _QuizScreenState extends State<QuizScreen> {
   }
 
   Widget QuizBody(DarkThemeProvider themeProvider) {
-
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
+        backgroundColor: Theme.of(context).backgroundColor,
         appBar: AppBar(
           title: Text("Quiz: Read & Write",
               style: TextStyle(
@@ -114,53 +113,3 @@ class QuizCard extends StatelessWidget {
         ));
   }
 }
-
-class QuizCard extends StatelessWidget {
-  final String description;
-
-  const QuizCard(this.description);
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => QuizScreen()),
-          );
-        },
-        child: Container(
-          height: 80,
-          margin: const EdgeInsets.all(10),
-          padding: const EdgeInsets.only(left: 10, right: 25),
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Text(description,
-                            textAlign: TextAlign.center,
-                            softWrap: true,
-                            style: TextStyle(
-                                fontFamily: 'OpenSans',
-                                color: Color(0xFF424242),
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold)),
-                      ])),
-            ],
-          ),
-          decoration: BoxDecoration(
-            color: Color(0xFFC5E1A5),
-            boxShadow: [
-              BoxShadow(
-                  blurRadius: 8.0,
-                  offset: Offset(-3.0, 3.0),
-                  color: Colors.grey),
-            ],
-            borderRadius: BorderRadius.all(Radius.circular(26)),
-          ),
-        ));
-    }
-  }
-
