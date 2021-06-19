19fb730c3adbb2a3988bfc9aaa9bcc210999681c
Ardel Anton Bontilao <45665876+ards678@users.noreply.github.com>
2021-06-19 11:42:42 +0800

    Revert "Changed email message"

--- android/app/src/main/kotlin/com/ssb/hachingu/MainActivity.kt
+++ android/app/src/main/kotlin/com/ssb/hachingu/MainActivity.kt
@@ -1,18 +1,6 @@
 package com.ssb.hachingu
 
-//import io.flutter.embedding.android.FlutterActivity
-
-//class MainActivity: FlutterActivity() {
-//}
-
-import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-
-    }
 }
